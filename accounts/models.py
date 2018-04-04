67765899514bb6f4529cce6974b32122c4dacaad
khmkii <james_mccombie@hotmail.com>
2018-04-04 18:44:43 +0100

    Declared models Account, Journal, and Posting in accounts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    pass
+
+
+class Journal(models.Model):
+    pass
+
+
+class Posting(models.Model):
+    pass
