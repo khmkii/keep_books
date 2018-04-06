c3fbd106bd45d94ab02c30074f549dc7070ea27b
khmkii <james_mccombie@hotmail.com>
2018-04-06 10:06:13 +0100

    Added BaseAccountModel with modified and creation timestamp fields -  Account, Journal, Posting Inherit from this

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
 
-class Account(models.Model):
+class BaseAccountModel(models.Model):
+    creation_timestamp = models.DateTimeField(editable=False)
+    modified_timestamp = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        """
+        On save update modified timestamp, and set creation timestamp if row does not exist
+        """
+
+        if not self.id:
+            self.creation_timestamp = timezone.now()
+        self.modified = timezone.now()
+
+        return super(BaseAccountModel, self).save(*args, **kwargs)
+
+
+class Account(BaseAccountModel):
     pass
 
 
-class Journal(models.Model):
+class Journal(BaseAccountModel):
     pass
 
 
-class Posting(models.Model):
+class Posting(BaseAccountModel):
     pass
