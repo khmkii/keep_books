324c78f0effaaff0c2d9e10d2e9b6168f295c20d
khmkii <james_mccombie@hotmail.com>
2018-04-16 09:16:18 +0100

    registered account app models in admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account, AssetType, Journal, Posting
+
+
+@admin.register(Account)
+class SalesContractAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AssetType)
+class SalesContractAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Journal)
+class SalesContractAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Posting)
+class SalesContractAdmin(admin.ModelAdmin):
+    pass
