d32e2f99e131a34cd1cdeb42841b7780b1e7714f
khmkii <james_mccombie@hotmail.com>
2018-05-09 14:46:06 +0100

    Added an accounts list view, added links for journal entry and accounts list views on accounts home view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from accounts.views import AccountsHomeView, JournalEntryView
+from accounts.views import AccountsHomeView, AccountsListView, JournalEntryView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path(r'', AccountsHomeView.as_view(), name='home'),
-    path(r'journal', JournalEntryView.as_view())
+    path(r'journal', JournalEntryView.as_view(), name='journal_entry'),
+    path(r'accounts-list', AccountsListView.as_view(), name='accounts_list'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,26 @@
+from django.contrib import messages
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db import IntegrityError, transaction
 from django.forms import modelformset_factory
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.shortcuts import redirect, render
+from django.utils import timezone
+from django.views.generic import ListView, TemplateView, View
+import uuid
 
 from accounts.forms import BaseJournalFormSet
-from accounts.models import Journal
+from accounts.models import Account, Journal
 
 
 class AccountsHomeView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/accounts_home.html'
+
+
+class AccountsListView(LoginRequiredMixin, ListView):
+    template_name = 'accounts/accounts_list.html'
+    model = Account
+    context_object_name = 'accounts'
 
 
 class JournalEntryView(LoginRequiredMixin, View):
@@ -24,12 +35,14 @@ class JournalEntryView(LoginRequiredMixin, View):
             ),
             formset=BaseJournalFormSet,
             max_num=25,
-            extra=1
+            extra=2
         )
     template_name = 'accounts/journal_entry.html'
 
     def get(self, request, *args, **kwargs):
+
         journal_formset = self.JournalFormset(queryset=Journal.objects.none())
+
         return render(
             request=request,
             template_name=self.template_name,
@@ -38,6 +51,24 @@ class JournalEntryView(LoginRequiredMixin, View):
             }
         )
 
-    def post(self):
-        pass
+    def post(self, request, *args, **kwargs):
+
+        unique_identifier = str(uuid.uuid4())
+
+        journal_formset = self.JournalFormset(data=request.POST)
+
+        if journal_formset.is_valid():
+
+            new_entries = []
+
+            for journal_entry in journal_formset:
+                new_entries.append(journal_entry)
+
+            try:
+                with transaction.atomic():
+                    Journal.objects.bulk_create(new_entries)
+                    messages.success(request, message="Journal entries added")
+            except IntegrityError:
+                messages.error(request, "Error - please check journal entries balance")
+                return redirect(reverse("accounts:journal_entry"))
 
